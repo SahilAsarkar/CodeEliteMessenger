304291f531fdd312d384e50f08eb14a0f25e9b98
Sahil <sahilasarkar3@gmail.com>
2026-03-08 20:10:04 +0530

    Fixed Bugs

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://juoonqfkknbrcxnkylqp.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1b29ucWZra25icmN4bmt5bHFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MTMwMTksImV4cCI6MjA4ODM4OTAxOX0.kBPYnb2cv6TexZJWffUs41rQM_55csFkqNAK95KJ-IA"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
